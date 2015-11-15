45159bd93729e6a428c0e173f3db1a9ed8cee28f
Andrew Ellison <aellison1558@gmail.com>
2015-11-14 21:56:28 -0800

    Weekend work

--- PollApp/app/models/question.rb
+++ PollApp/app/models/question.rb
@@ -1,7 +1,34 @@
 class Question < ActiveRecord::Base
-
+  validates :poll_id, :text, presence: true
   belongs_to :poll, class_name: 'Poll', foreign_key: :poll_id, primary_key: :id
   has_many :answer_choices, class_name: 'AnswerChoice', foreign_key: :question_id, primary_key: :id
+  has_many :responses, through: :answer_choices, source: :responses
 
+  def results_n_plus_1
+    choices = answer_choices
+    results = {}
+    choices.each do |choice|
+      results[choice.text] = choice.responses.count
+    end
+    results
+  end
+
+  def results
+    choices = answer_choices.includes(:responses)
+    results = {}
+    choices.each do |choice|
+      results[choice.text] = choice.responses.count
+    end
+    results
+  end
+
+  def results
+    choices = answer_choices.joins(:responses).group(:id).select(:text, :count)
+    results = {}
+    choices.each do |choice|
+      results[choice.text] = choice.count
+    end
+    results
+  end
 
 end
--- PollApp/app/models/response.rb
+++ PollApp/app/models/response.rb
@@ -1,6 +1,30 @@
 class Response < ActiveRecord::Base
-
+  validates :user_id, :answer_choice_id, presence: true
+  validate :respondent_has_not_already_answered_question
+  validate :user_is_not_author
   belongs_to :user, class_name: 'User', foreign_key: :user_id, primary_key: :id
   belongs_to :answer_choice, class_name: 'AnswerChoice', foreign_key: :answer_choice_id, primary_key: :id
-  
+  has_one :question, through: :answer_choice, source: :question
+
+
+  def sibling_responses
+    siblings = question.responses
+    if id
+      siblings.where("id != #{self.id}")
+    else
+      siblings
+    end
+  end
+
+  def respondent_has_not_already_answered_question
+    unless sibling_responses.where(user_id: user_id).empty?
+      errors[:user_id] << "User has already answered question!"
+    end
+  end
+
+  def user_is_not_author
+    if question.poll.author_id == user_id
+      errors[:user_id] << "Author can't answer own poll!"
+    end
+  end
 end
